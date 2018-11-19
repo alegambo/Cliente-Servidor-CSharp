4afbe46077c68f00da2e9860357b916b9fda586b
unknown <alegamboaba.17@gmail.com>
2018-11-19 02:01:43 -0600

    Se agrega proyecto

---
+++ Cliente-Servidor/Cliente/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Clientes
+{
+    class Program
+    {
+        private static readonly Socket SocketCliente = new Socket
+            (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        private const int PORT = 100;
+
+        static void Main()
+        {
+            Console.Title = "Jugador";
+            ConectarAServer();
+            MantenerseEnSocket();
+            Salir();
+        }
+
+        private static void ConectarAServer()
+        {
+            int intentos = 0;
+
+            while (!SocketCliente.Connected) 
+            {
+                try
+                {
+                    intentos++;
+                    Console.WriteLine("Intentando conexion" + intentos);
+                    SocketCliente.Connect(IPAddress.Loopback, PORT);
+                }
+                catch (SocketException) 
+                {
+                    Console.Clear();
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("Conectado");
+        }
+
+        private static void MantenerseEnSocket()
+        {
+            Console.WriteLine(@"envie su nombre de usuario");
+            Console.WriteLine(@"Digite Salir para desconectarse");
+
+            while (true)
+            {
+                EnviaRespuesta();
+                RecibirRespuesta();
+            }
+        }
+
+       
+        private static void Salir()
+        {
+            EnviarMensaje("salir");
+            SocketCliente.Shutdown(SocketShutdown.Both);
+            SocketCliente.Close();
+            Environment.Exit(0);
+        }
+
+        private static void EnviaRespuesta()
+        {
+            Console.Write("Envia el mensaje al servidor: ");
+            string request = Console.ReadLine();
+            EnviarMensaje(request);
+
+            if (request.ToLower() == "salir")
+            {
+                Salir();
+            }
+        }
+
+        
+        private static void EnviarMensaje(string mensaje)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(mensaje);
+            SocketCliente.Send(buffer, 0, buffer.Length, SocketFlags.None);
+        }
+
+        private static void RecibirRespuesta()
+        {
+            var buffer = new byte[2048];
+            int received = SocketCliente.Receive(buffer, SocketFlags.None);
+            if (received == 0) return;
+            var data = new byte[received];
+            Array.Copy(buffer, data, received);
+            string text = Encoding.ASCII.GetString(data);
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ Cliente-Servidor/Servidor/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Servidor
+{
+    class Program
+    {
+        private static readonly Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static readonly List<Socket> clienteSockets = new List<Socket>();
+        private const int BUFFER_SIZE = 2048;
+        private const int PORT = 100;
+        private static readonly byte[] buffer = new byte[BUFFER_SIZE];
+        private static readonly List<String> listajugadores = new List<string>() { "jugador1", "jugador2", "jugador3"};
+
+        static void Main()
+        {
+            Console.Title = "Servidor";
+            IniciarSocket();
+            Console.ReadLine(); // When we press enter close everything
+            CerrarSockets();
+        }
+
+        private static void IniciarSocket()
+        {
+            Console.WriteLine("Iniciando servidor...");
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, PORT));
+            serverSocket.Listen(0);
+            serverSocket.BeginAccept(AceptarSocket, null);
+            Console.WriteLine("Servidor corriendo");
+        }
+
+       
+        private static void CerrarSockets()
+        {
+            foreach (Socket socket in clienteSockets)
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+
+            serverSocket.Close();
+        }
+
+        private static void AceptarSocket(IAsyncResult AR)
+        {
+            Socket socket;
+
+            try
+            {
+                socket = serverSocket.EndAccept(AR);
+            }
+            catch (ObjectDisposedException) 
+            {
+                return;
+            }
+          
+               
+                    clienteSockets.Add(socket);
+                    socket.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, RecibirLlamado, socket);
+                    Console.WriteLine("Detectamos una nueva conexion,mensaje del jugador...");
+                    serverSocket.BeginAccept(AceptarSocket, null);
+                
+                }
+        
+        
+
+        private static void RecibirLlamado(IAsyncResult AR)
+        {
+            Socket current = (Socket)AR.AsyncState;
+            int received;
+
+            try
+            {
+                received = current.EndReceive(AR);
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Jugador forzadamente desconectado");
+               
+                current.Close(); 
+                clienteSockets.Remove(current);
+                return;
+            }
+
+            byte[] recBuf = new byte[received];
+            Array.Copy(buffer, recBuf, received);
+            string text = Encoding.ASCII.GetString(recBuf);
+            Console.WriteLine("Recibiendo mensaje: " + text);
+
+           if (text.ToLower() == "salir")
+            {
+                
+                current.Shutdown(SocketShutdown.Both);
+                current.Close();
+                clienteSockets.Remove(current);
+                Console.WriteLine("Jugador Desconectado");
+                return;
+            }
+          
+
+            current.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, RecibirLlamado, current);
+        }
+    }
+}
